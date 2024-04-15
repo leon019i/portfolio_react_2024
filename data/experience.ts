2f1d872be258e4eb9bacd9c3eb42dc0710be7e2f
leon019i <leonlord0@gamil.com>
2024-04-15 04:06:36 +0200

    final temp

--- data/experience.ts
+++ data/experience.ts
@@ -1,17 +1,25 @@
 export const experienceData = [
+    // {
+    //     date: "2023 - Now",
+    //     title: "Software QA Engineer",
+    //     subTitle: "Inspire for Solutions Development",
+    //     link: "https://inspirejo.com/",
+    //     tag: "On-Site",
+    // }, {
+    //     date: "2022 - Now",
+    //     title: "Software Engineer (Django)",
+    //     subTitle: "Freelance",
+    //     link: "https : /",
+    //     tag: "Remote",
+    // }
     {
         date: "2023 - Now",
-        title: "Software QA Engineer",
-        subTitle: "Inspire for Solutions Development",
-        link: "https://inspirejo.com/",
-        tag: "On-Site",
-    }, {
-        date: "2022 - Now",
-        title: "Software Engineer (Django)",
-        subTitle: "Freelance",
+        title: "Software Engineer",
+        // subTitle: "Freelance",
         link: "https : /",
-        tag: "Remote",
-    },{
+        tag: "On-Site",
+    }
+    ,{
         date: "2021 - 2022",
         title: "Wordpress Moderator ",
         subTitle: "ArtsNile",
