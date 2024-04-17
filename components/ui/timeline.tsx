7dec62ad015dc16eabc819f36630b69fb8802d58
leon019i <leonlord0@gamil.com>
2024-04-17 04:54:35 +0200

    fixing the fitting

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -34,14 +34,14 @@ export const TimelineItem: React.FC<TimelineItemProps> = ({ date, title, subTitl
     return (
         <div className="flex flex-wrap gap-12 min-h justify-start relative text-base sm:text-md">
             {/* Date Timeline */}
-            <div className="h-auto flex-none whitespace-pre" style={{ width: `${isCourse ? "0" : "auto"}` }}>
+            <div className="h-auto w-fit flex-none whitespace-pre" style={{ width: `${isCourse ? "0" : "auto"}` }}>
                 <p className="text-secondary-foreground">{date}</p>
             </div>
             {/* Right side */}
-            <div className="flex gap-x-2" style={{ transform: `${isCourse ? "translateX(-45px)" : "none"}` }}>
+            <div className="flex gap-x-2 w-fit" style={{ transform: `${isCourse ? "translateX(-45px)" : "none"}` }}>
                 <div className="flex flex-col gap-y-0.5">
                     {/* Title */}
-                    <div className="break-words whitespace-pre">
+                    <div className="break-words w-fit whitespace-pre">
                         <p className="leading-6 font-medium">{title}</p>
                     </div>
                     {/* Subtitle */}
@@ -69,7 +69,7 @@ interface BodyProps {
 const Body: React.FC<BodyProps> = ({ subTitle, tag, link }) => {
     return (
         <div className="flex items-center">
-            <p className="text-lg font-normal leading-6 mt-1">{subTitle}</p>
+            <p className="text-lg w-fit font-normal leading-6 mt-1">{subTitle}</p>
             {link && <FiArrowUpRight className="text-lg" />}
             {tag && (
                 <div className="ml-2 rounded-full bg-white/5 py-0.5 px-1.5">
