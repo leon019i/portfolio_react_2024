2b5cbc953e41c2e805f08a70b3a53c7348aec8e0
leon019i <leonlord0@gamil.com>
2024-04-17 03:15:25 +0200

    applying the SEO needed for google seo to get more reach and ranked up and boost it

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Head from 'next/head'
 import dynamic from "next/dynamic";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
@@ -13,6 +13,9 @@ const WaterWaveWrapper = dynamic(
     () => import("@/components/visualEffects/water-wave-wrapper"),
     { ssr: false }
 );
+
+
+
 
 export default function Home() {
     // const [play, { stop }] = useSound('/assets/music/TheWeeknd_AfterHours.mp3', {
@@ -28,23 +31,45 @@ export default function Home() {
     //     return () => stop();
     // }, [play, stop]);  // Dependency array includes play and stop
 
-    return (
-        <WaterWaveWrapper
-            imageUrl=""
-            dropRadius="3"
-            perturbance="3"
-            resolution="2048"
-        >
-            {() => (
-                <div className="pb-8">
-                    <Analytics/>
-                    <SpeedInsights/>
-                    <LandingSection />
-                    <FeaturedSection />
-                    <AboutSection />
-                    <ContactSection />
-                </div>
-            )}
-        </WaterWaveWrapper>
+    return (<>
+            <Head>
+                <title>Loay Fayez | Software & QA Engineer | Portfolio</title>
+                <meta name="description"
+                      content="Discover Loay Fayez's journey from a BSc in Computer Science & AI at Cairo University through roles in software QA and Django development. Explore my projects, read about my experiences and view my certifications."/>
+                <meta name="keywords"
+                      content="Loay Fayez, Software QA Engineer, QA Engineer, ISTQB, Django Developer, Python, JavaScript, Software Engineer, Backend Developer, Freelance Developer, Cairo University, Computer Science, Artificial Intelligence, Web Development, QA Testing"/>
+                <meta property="og:title" content="Loay Fayez - Software QA & Software Engineer"/>
+                <meta property="og:description"
+                      content="Portfolio of Loay Fayez, showcasing skills in Software Engineering, QA, Django, and backend development alongside projects that highlight continuous learning and proficiency in modern technologies."/>
+                <meta property="og:url" content="https://www.louay.live"/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:image" content="https://www.louay.live/favicon.ico"/>
+                <link rel="canonical" href="https://www.louay.live"/>
+                <link rel="icon" href="/favicon.ico"/>
+
+                {/* Replace with your actual OG image URL */}
+                {/*<meta name="twitter:card" content="summary_large_image" />*/}
+                {/*<meta name="twitter:title" content="Loay Fayez - Portfolio" />*/}
+                {/*<meta name="twitter:description" content="Discover Loay Fayez's professional and personal projects through his official portfolio website. Click to learn more!" />*/}
+                {/*<meta name="twitter:image" content="https://www.louay.live/twitter-og-image.jpg" />  /!* Replace with your actual Twitter image URL *!/*/}
+            </Head>
+            <WaterWaveWrapper
+                imageUrl=""
+                dropRadius="3"
+                perturbance="3"
+                resolution="2048"
+            >
+                {() => (
+                    <div className="pb-8">
+                        <Analytics/>
+                        <SpeedInsights/>
+                        <LandingSection/>
+                        <FeaturedSection/>
+                        <AboutSection/>
+                        <ContactSection/>
+                    </div>
+                )}
+            </WaterWaveWrapper>
+        </>
     );
 }
